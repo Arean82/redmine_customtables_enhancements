810177d3530160f447429592ff5847520490df16
arean82 <arean82@gmail.com>
2025-10-31 11:18:09 +0530

    updated

--- lib/redmine_customtables_enhancements.rb
+++ lib/redmine_customtables_enhancements.rb
@@ -1,7 +1,15 @@
-module RedmineCustomtablesEnhancements
-end
+#module RedmineCustomtablesEnhancements
+#end
+#
+#require_dependency 'redmine_customtables_enhancements/custom_entity_patch'
+#require_dependency 'redmine_customtables_enhancements/controller_patch'
+#require_dependency 'redmine_customtables_enhancements/hooks/view_issues_show_details_bottom_hook'
 
-require_dependency 'redmine_customtables_enhancements/custom_entity_patch'
-require_dependency 'redmine_customtables_enhancements/controller_patch'
-require_dependency 'redmine_customtables_enhancements/hooks/view_issues_show_details_bottom_hook'
+# frozen_string_literal: true
+
+plugin_root = File.expand_path('..', __dir__)
+
+require File.join(plugin_root, 'redmine_customtables_enhancements', 'custom_entity_patch')
+require File.join(plugin_root, 'redmine_customtables_enhancements', 'controller_patch')
+require File.join(plugin_root, 'redmine_customtables_enhancements', 'hooks', 'view_issues_show_details_bottom_hook')
 
