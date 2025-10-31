c7076f44113d97e6d04caa537a2329db73c6a1f4
arean82 <arean82@gmail.com>
2025-10-31 11:19:26 +0530

    updated

--- lib/redmine_customtables_enhancements.rb
+++ lib/redmine_customtables_enhancements.rb
@@ -9,7 +9,9 @@
 
 plugin_root = File.expand_path('..', __dir__)
 
-require File.join(plugin_root, 'redmine_customtables_enhancements', 'custom_entity_patch')
-require File.join(plugin_root, 'redmine_customtables_enhancements', 'controller_patch')
-require File.join(plugin_root, 'redmine_customtables_enhancements', 'hooks', 'view_issues_show_details_bottom_hook')
+# Load patch files directly from the lib directory
+require File.join(PLUGIN_ROOT, 'lib', 'redmine_customtables_enhancements', 'custom_entity_patch')
+require File.join(PLUGIN_ROOT, 'lib', 'redmine_customtables_enhancements', 'controller_patch')
+require File.join(PLUGIN_ROOT, 'lib', 'redmine_customtables_enhancements', 'hooks', 'view_issues_show_details_bottom_hook')
+
 
